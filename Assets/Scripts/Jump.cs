bbcbb9a3e17a20bead6e0c0c0d0ca26b4c52077c
iklimapaydin <iklimapaydin@gmail.com>
2025-07-06 15:46:01 +0300

    Jump Scripti eklendi

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class JumpOnly : MonoBehaviour
+{
+    public float jumpForce = 15f;
+    private Rigidbody rb;
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
+
